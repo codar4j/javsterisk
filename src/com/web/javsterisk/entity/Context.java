9af920fdbcbba19d0d6d3fc628a8914749175d71
Freddy Moran <fmoran@codar4j.com>
2017-01-05 04:39:48 -0300

    New context option CRUD
    Change-Id: Icc62641f0b4a81f52e1f369c043fea35ac0251a2
    Signed-off-by: Freddy Moran <fmoran@codar4j.com>

---
+++ src/com/web/javsterisk/entity/Context.java
@@ -0,0 +1,101 @@
+package com.web.javsterisk.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * 
+ * @author Freddy Moran
+ *
+ */
+@Entity
+@Table(name = "context", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
+public class Context implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(max = 20)	
+	@Column(name = "name", nullable = false, length = 20)
+	private String name;
+	
+	@NotNull
+	@Size(max = 128)	
+	@Column(name = "detail", nullable = false, length = 128)
+	private String detail;
+
+	public Context() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Context))
+			return false;
+		Context other = (Context) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Context [id=" + id + ", name=" + name + ", detail=" + detail + "]";
+	}
+	
+}
