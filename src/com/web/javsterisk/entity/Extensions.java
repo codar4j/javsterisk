5c354799ec8394ad320e0cbdf6c69d1f434c95e3
Freddy Moran <fmoran@codar4j.com>
2017-02-01 17:21:11 -0300

    chamges in view dial plan, issue #21
    Signed-off-by: Freddy Moran <fmoran@codar4j.com>

--- src/com/web/javsterisk/entity/Extensions.java
+++ src/com/web/javsterisk/entity/Extensions.java
@@ -10,7 +10,6 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
